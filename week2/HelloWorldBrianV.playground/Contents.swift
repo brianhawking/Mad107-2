82b25da444dbda51af62b823569954b3a778b819
brianhawking <beveitch@gmail.com>
2021-01-24 16:22:01 -0600

    Update Contents.swift

--- week2/HelloWorldBrianV.playground/Contents.swift
+++ week2/HelloWorldBrianV.playground/Contents.swift
@@ -21,4 +21,4 @@ print(name1, name2, name3, name4, separator: ", ", terminator:"\n", to:&line)
 print(line)
 
 // I'm adding something here to practice with github
-print(line,line)
+print(line,line, line)
